24164b1dcdedeeabed1addb4990412d82e98a51e
Eric Miller <eric@legoaces.org>
2020-04-16 09:59:15 -0400

    Create clang-format@8.rb

---
+++ Formula/clang-format@8.rb
@@ -0,0 +1,70 @@
+class ClangFormatAT8 < Formula
+  desc "Formatting tool for C/C++/Java/JavaScript/Objective-C/Protobuf"
+  homepage "https://releases.llvm.org/8.0.0/tools/clang/docs/ClangFormat.html"
+  version "8.0.0"
+
+  if MacOS.version >= :sierra
+    url "https://llvm.org/svn/llvm-project/llvm/tags/RELEASE_800/final/", :using => :svn
+  else
+    url "http://llvm.org/svn/llvm-project/llvm/tags/RELEASE_800/final/", :using => :svn
+  end
+
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
+  depends_on "subversion" => :build
+
+  resource "clang" do
+    if MacOS.version >= :sierra
+      url "https://llvm.org/svn/llvm-project/cfe/tags/RELEASE_800/final/", :using => :svn
+    else
+      url "http://llvm.org/svn/llvm-project/cfe/tags/RELEASE_800/final/", :using => :svn
+    end
+  end
+
+  resource "libcxx" do
+    url "https://releases.llvm.org/8.0.0/libcxx-8.0.0.src.tar.xz"
+    sha256 "c2902675e7c84324fb2c1e45489220f250ede016cc3117186785d9dc291f9de2"
+  end
+
+  def install
+    (buildpath/"projects/libcxx").install resource("libcxx")
+    (buildpath/"tools/clang").install resource("clang")
+
+    mkdir "build" do
+      args = std_cmake_args
+      args << "-DCMAKE_OSX_SYSROOT=/" unless MacOS::Xcode.installed?
+      args << "-DLLVM_ENABLE_LIBCXX=ON"
+      args << ".."
+      system "cmake", "-G", "Ninja", *args
+      system "ninja", "clang-format"
+      bin.install "bin/clang-format"
+    end
+    bin.install "tools/clang/tools/clang-format/git-clang-format"
+    (share/"clang").install Dir["tools/clang/tools/clang-format/clang-format*"]
+  end
+
+  test do
+    # NB: below C code is messily formatted on purpose.
+    (testpath/"test.c").write <<~EOS
+      int         main(char *args) { \n   \t printf("hello"); }
+    EOS
+
+    assert_equal "int main(char *args) { printf(\"hello\"); }\n",
+        shell_output("#{bin}/clang-format -style=Google test.c")
+
+    # below code is messily formatted on purpose.
+    (testpath/"test2.h").write <<~EOS
+      #import  "package/file.h"
+      @interface SomePlugin   : NSObject  < ParentPlugin >
+      @end
+    EOS
+
+    # NOTE! different formatting depending on version
+    # clang-format 5.x
+    #     @interface SomePlugin : NSObject<ParentPlugin>
+    # clang-format 6.x, 7.x
+    #     @interface SomePlugin : NSObject <ParentPlugin>
+    assert_equal "#import \"package/file.h\"\n@interface SomePlugin : NSObject <ParentPlugin>\n@end\n",
+        shell_output("#{bin}/clang-format -style=Google test2.h")
+  end
+end
